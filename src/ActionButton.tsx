bcf93e435654135e8a2914cf680be45e9fb9e5d4
trey <trey@datacapable.com>
2023-11-07 20:43:29 -0600

    Update the metadata and button styling

--- src/ActionButton.tsx
+++ src/ActionButton.tsx
@@ -3,6 +3,6 @@ import { Link } from "react-router-dom";
 
 export default function ActionButton() {
     return <div className="actionButton">
-        <Link to={"/bio"} className="link">Click Here to Meet the Coach</Link>
+        <Link to={"/bio"} className="link">Meet the Coach</Link>
     </div>
 }
