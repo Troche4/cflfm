9b48d8aafe0e978c131f57fdd54761a6a665e787
trey <trey@datacapable.com>
2023-11-19 23:31:50 -0600

    WIP use logo instead of text

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return <div>
         <div className="navbar">
-            <div className="logo"><Link to="/" className="link"></Link></div>
-            <Link to="/" className="linkBanner"><div>Coaching for</div><div>Law Firm Managers</div></Link>
-            <div/>
+            <div className="logo"><Link to="/" className="link"><img src="./logo.jpg"/></Link></div>
         </div>
     </div>
 }
