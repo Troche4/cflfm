ff9b06feb4977d49280d46640c60087198f8b93d
trey <trey@datacapable.com>
2023-11-26 18:48:24 -0600

    add transparent logo to navbar

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return <div>
         <div className="navbar">
-            <div className="logo"><Link to="/" className="link"><img src="./logo.jpg"/></Link></div>
+            <Link to="/" className="link"><img src="./remove-bg.png"/></Link>
         </div>
     </div>
 }
