db96d96e635b3aa088855d5c3eea6d64df3753cd
trey <trey@datacapable.com>
2023-12-23 13:06:19 -0500

    Add more content to the landing page

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,7 +9,30 @@ export default function Home() {
         </div>
 
         <div className="homeMain">
+            <div className="sidebar">
+                <div>
+                    <div className="listHeader">Management is Tough Stuff - we will help you:</div>
+                    <ul>
+                        <li>Have Difficult Conversations</li>
+                        <li>Engage in Healthy Conflict</li>
+                        <li>Strive for an <i>All for One and One for All</i> Firm</li>
+                        <li>Change Behaviors for Yourself and Others</li>
+                        <li>Hold Yourself and Others Accountable</li>
+                        <li>Plan and Execute</li>
+                        <li>Strategic Planning; Time Management; Work/Life Balance</li>
+                        <ul>
+                            <li>
+                            In short,  we will help you solve your firm's most vexing* challenges.<br/><br/>
+                            * vexing  /'veksiNG/  adjective  causing annoyance, frustration or worry.
+                            </li>
+                        </ul>
+                    </ul>
+                    
+                </div>
+            </div>
+
             <img src="./consulting_hero.jpg" className="hero"/>
+
             <div className="sidebar">
                 <div>
                     <div className="listHeader">Our Commitment</div>
